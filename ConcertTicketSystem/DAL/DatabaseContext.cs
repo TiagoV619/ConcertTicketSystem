d05422beee863a9415cec462012c9dc0a8185c40
SANTIAGO VARELA JIM�NEZ <santiagovarela314184@correo.itm.edu.co>
2023-04-01 22:29:58 -0500

    I created the DAL folder with its Tickets entity, synchronized the database with the code and created the controller for the respective entity.

---
+++ ConcertTicketSystem/DAL/DatabaseContext.cs
@@ -0,0 +1,13 @@
+﻿using ConcertTicketSystem.DAL.Entity;
+using Microsoft.EntityFrameworkCore;
+namespace ConcertTicketSystem.DAL
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Ticket> Tickets { get; set; }
+    }
+}
---
+++ ConcertTicketSystem/DAL/Entity/Ticket.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace ConcertTicketSystem.DAL.Entity
+{
+    public class Ticket
+    {
+        [Key]
+        [Required]
+        public Guid Id { get; set; }
+
+        [Display(Name = "Fecha de ingreso")]
+        public DateTime? UserDate { get; set; }
+
+        [Display(Name = "estado")]
+        public bool IsUsed { get; set; }
+
+
+        [Display(Name = "Porteria de ingreso")]
+        public string? EntranceGate { get; set; }
+    }
+}
